3719766ca6c87bdc236e2dd2ae4d76648107e652
shichaoj <504315264@qq.com>
2016-12-12 10:27:18 +0800

    12.12

--- app/js/services.js
+++ app/js/services.js
@@ -4,10 +4,11 @@
 
 // angular.module 函数创建模块
 var phonecatServices = angular.module('phonecatServices', ['ngResource']);
-
-phonecatServices.factory('Phone', ['$resource',
+// ngResource 模块包含$resource 服务
+phonecatServices.factory('Phone', ['$resource',  //避免压缩问题
   function($resource){
     return $resource('phones/:phoneId.json', {}, {
+      //isArray ：boolean，如果为true，那么这个行为返回的对象是个数组
       query: {method:'GET', params:{phoneId:'phones'}, isArray:true}
     });
   }]);
